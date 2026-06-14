3d486ff2a83ed385a0f27be4a296359051f674dd
Yashaswee Mishra <146866918+YASHMGP69420@users.noreply.github.com>
2026-06-14 21:32:23 +0530

    debugging

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -30,7 +30,7 @@ app.post("/create-post", upload.single("image"), async (req,res)=>{
 app.get("/", async (req,res)=>{
     const posts = await postModel.find()
 
-    return res.status(200).json({
+        res.status(200).json({
         message:"posts fethed",
         posts
     })
